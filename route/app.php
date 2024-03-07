948183fa207762123557a1a9ccc5491b1fce68c9
黎笔家 <92807337+lee-bj@users.noreply.github.com>
2024-03-07 09:30:03 +0800

    重现路由

--- route/app.php
+++ route/app.php
@@ -1,17 +1,13 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+/**
+ * 管理对应模块-路由文件
+ * 管理路径：app\center\controller
+ * User Long
+ * Date 2023/06/27
+ */
+
 use think\facade\Route;
 
-Route::get('think', function () {
-    return 'hello,ThinkPHP6!';
+Route::get('/', function () {
+    return 'hello,word!';
 });
-
-Route::get('hello/:name', 'index/hello');
